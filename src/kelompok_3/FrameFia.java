4437242d4cd851662615bd4930ffbcd43dbb51ac
sindufha <sindufha215@gmail.com>
2025-09-11 16:20:32 +0700

    Add Frame Ana

--- src/kelompok_3/FrameFia.java
+++ src/kelompok_3/FrameFia.java
@@ -8,12 +8,12 @@ package kelompok_3;
  *
  * @author MyBook Hype AMD
  */
-public class Charis extends javax.swing.JFrame {
+public class FrameFia extends javax.swing.JFrame {
 
     /**
-     * Creates new form Charis
+     * Creates new form FrameFia
      */
-    public Charis() {
+    public FrameFia() {
         initComponents();
     }
 
@@ -59,20 +59,20 @@ public class Charis extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Charis.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrameFia.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Charis.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrameFia.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Charis.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrameFia.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Charis.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrameFia.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Charis().setVisible(true);
+                new FrameFia().setVisible(true);
             }
         });
     }
